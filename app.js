5b2e62080daf7afb8f40d411147a5053b6dd3ca9
Saeed Alsharqawi <saed_sharqawi@hotmail.com>
2020-02-04 08:23:58 +0300

    create the Folder structure, views, Routes, Models

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 //const expressHbs = require('express-handlebars')
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -17,17 +18,15 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
+const routes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
